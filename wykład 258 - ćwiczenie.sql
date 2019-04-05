8a1aa911db9fe9c47da35bb894be75ad46cc6f30
Adaxar101 <adamgajda94@gmail.com>
2019-04-05 09:18:38 +0200

    Test6

--- wykład 258 - ćwiczenie.sql
+++ wykład 258 - ćwiczenie.sql
@@ -1,14 +1,12 @@
-SELECT 
+USE employees_mod;
 
+SELECT 
     YEAR(d.from_date) AS calendar_year,
     e.gender,     
     COUNT(e.emp_no) AS num_of_employees
-
 FROM     
      t_employees e         
           JOIN     
      t_dept_emp d ON d.emp_no = e.emp_no
-
-GROUP BY calendar_year , e.gender 
-
+GROUP BY calendar_year, e.gender
 HAVING calendar_year >= 1990;
